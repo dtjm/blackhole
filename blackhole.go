f2f5cdce9fdcc7a96dce07ab7d9ef7129760c19e
Sam Nguyen <sam.nguyen@sendgrid.com>
2013-11-01 20:28:31 +0000

    Add TCP echo server

--- blackhole.go
+++ blackhole.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"github.com/bradfitz/go-smtpd/smtpd"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"strings"
@@ -12,8 +14,9 @@ import (
 	"time"
 )
 
-var smtpAddrs *string = flag.String("smtp", ":25", "SMTP adddresses")
-var httpAddrs *string = flag.String("http", ":80", "HTTP addresses")
+var smtpAddrs *string = flag.String("smtp", "", "SMTP adddresses")
+var httpAddrs *string = flag.String("http", "", "HTTP addresses")
+var tcpAddrs *string = flag.String("tcp", "", "Command server addresses")
 
 // Just dump the mail
 func onNewMail(c smtpd.Connection, from smtpd.MailAddress) (env smtpd.Envelope, err error) {
@@ -25,28 +28,34 @@ func onNewMail(c smtpd.Connection, from smtpd.MailAddress) (env smtpd.Envelope, 
 
 func main() {
 	flag.Parse()
-
-	addrs := strings.Split(*smtpAddrs, ",")
 	wg := sync.WaitGroup{}
 
-	for _, addr := range addrs {
-		wg.Add(1)
-		log.Printf("Listening on smtp://%s", addr)
-		go func() {
-			server := smtpd.Server{
-				Addr:         addr,
-				Hostname:     "", // use system hostname
-				ReadTimeout:  60 * time.Second,
-				WriteTimeout: 60 * time.Second,
-				PlainAuth:    false,
-				OnNewMail:    onNewMail,
-			}
-			err := server.ListenAndServe()
-			if err != nil {
-				log.Fatalf("Failed to start server: %s", err)
-			}
-			wg.Done()
-		}()
+	if *smtpAddrs == "" && *httpAddrs == "" && *tcpAddrs == "" {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	if *smtpAddrs != "" {
+		addrs := strings.Split(*smtpAddrs, ",")
+		for _, addr := range addrs {
+			wg.Add(1)
+			log.Printf("Listening on smtp://%s", addr)
+			go func() {
+				server := smtpd.Server{
+					Addr:         addr,
+					Hostname:     "", // use system hostname
+					ReadTimeout:  60 * time.Second,
+					WriteTimeout: 60 * time.Second,
+					PlainAuth:    false,
+					OnNewMail:    onNewMail,
+				}
+				err := server.ListenAndServe()
+				if err != nil {
+					log.Fatalf("Failed to start server: %s", err)
+				}
+				wg.Done()
+			}()
+		}
 	}
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
@@ -56,19 +65,50 @@ func main() {
 		fmt.Println()
 	})
 
-	addrs = strings.Split(*httpAddrs, ",")
-	for _, addr := range addrs {
-		wg.Add(1)
-		log.Printf("Listening on http://%s", addr)
-		go func() {
-			err := http.ListenAndServe(addr, nil)
-			if err != nil {
-				log.Fatalf("Failed to start HTTP server: %s", err)
-			}
-			wg.Done()
-		}()
+	if *httpAddrs != "" {
+		addrs := strings.Split(*httpAddrs, ",")
+		for _, addr := range addrs {
+			wg.Add(1)
+			log.Printf("Listening on http://%s", addr)
+			go func() {
+				err := http.ListenAndServe(addr, nil)
+				if err != nil {
+					log.Fatalf("Failed to start HTTP server: %s", err)
+				}
+				wg.Done()
+			}()
+		}
+	}
+
+	if *tcpAddrs != "" {
+		addrs := strings.Split(*tcpAddrs, ",")
+		for _, addr := range addrs {
+			wg.Add(1)
+			log.Printf("Listening on tcp://%s", addr)
+			go func() {
+				l, err := net.Listen("tcp", addr)
+				if err != nil {
+					log.Fatalf("Failed to start Command server: %s", err)
+				}
+				for {
+					conn, err := l.Accept()
+					if err != nil {
+						log.Printf("Error accepting on tcp://%s: %s", addr, err)
+					}
+
+					go func() {
+						scanner := bufio.NewScanner(conn)
+						for scanner.Scan() {
+							line := scanner.Text()
+							fmt.Printf("%s: %s\n", conn.RemoteAddr().String(), line)
+							conn.Write([]byte(line))
+							conn.Write([]byte{'\n'})
+						}
+					}()
+				}
+			}()
+		}
 	}
 
 	wg.Wait()
-
 }
